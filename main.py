a967c3e648cf74d166e91376561e0c6b6c918150
Molipow <molipowgamine@gmail.com>
2020-02-22 16:46:45 +0100

    Score system

--- main.py
+++ main.py
@@ -29,31 +29,43 @@ class Ball:
     def __init__(self, image, speed):
         self.image = image
         self.speed = speed
-        self.pos = Vector2(286, 187)
+        self.pos = Vector2(287, 187)
         self.width = 25
         self.height = 25
         self.direction = Vector2(1, 1)
-        self.cd = 0
     def move(self):
         self.pos.move(self.direction.x * self.speed, self.direction.y * self.speed)
         if self.pos.y + self.height > 400 or self.pos.y < 0:
             self.direction.y = -self.direction.y
     def detect_collision(self, _paddle):
-        self.cd -= 1
-        if _paddle.num == 1 and self.cd <=0 and self.pos.x + self.width > _paddle.pos.x and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height:
+        if (_paddle.num == 1 and self.pos.x + self.width > _paddle.pos.x and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height) or (_paddle.num == 0 and self.pos.x < _paddle.pos.x + _paddle.width and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height):
             self.direction.x = -self.direction.x
-            self.cd = 3
-        if _paddle.num == 0 and self.cd <=0 and self.pos.x < _paddle.pos.x + _paddle.width and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height:
-            self.direction.x = -self.direction.x
-            self.cd = 3
-    
+            # self.direction.y = _paddle.height/2
+    def check_for_win(self):
+        global score1, score2
+        if self.pos.x <= 0:
+            score2 += 1
+            reset()
+        if self.pos.x + self.width >= 600:
+            score1 += 1
+            reset() 
+
+def reset():
+    ball.pos = Vector2(287, 187)
+    ball.direction = Vector2(1, 1)
+    paddle1.pos = Vector2(20, 150)
+    paddle2.pos = Vector2(570, 150)
+
 pygame.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+score1, score2 = 0, 0
 size = width, height = 600, 400
 black = 0, 0, 0
 
+
 paddle1 = Paddle(pygame.image.load("paddle.png"), 0, 5)
 paddle2 = Paddle(pygame.image.load("paddle.png"), 1, 5)
-ball = Ball(pygame.image.load("ball.png"), 3)
+ball = Ball(pygame.image.load("ball.png"), 7)
 
 screen = pygame.display.set_mode(size)
 
@@ -74,8 +86,12 @@ while 1:
     ball.move()
     ball.detect_collision(paddle1)
     ball.detect_collision(paddle2)
+    ball.check_for_win()
+
+    scoretext = myfont.render(f'{score1}:{score2}', False, (255, 255, 255))
 
     screen.fill(black)
+    screen.blit(scoretext,(286,100))
     screen.blit(paddle1.image, (paddle1.pos.x, paddle1.pos.y))
     screen.blit(paddle2.image, (paddle2.pos.x, paddle2.pos.y))
     screen.blit(ball.image, (ball.pos.x, ball.pos.y))
