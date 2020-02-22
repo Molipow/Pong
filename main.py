c507fa26d55ea0837d0933ebed979a9abddfd63e
Molipow <molipowgamine@gmail.com>
2020-02-22 16:26:47 +0100

    Collision added

--- main.py
+++ main.py
@@ -6,29 +6,47 @@ class Vector2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def move(self, x, y):
+        self.x = self.x + x
+        self.y = self.y + y
 
 class Paddle:
     def __init__(self, image, num, speed):
         self.image = image
+        self.num = num
         self.speed = speed
-        self.pos = image.get_rect().move(num * 550 + 20, 150)
+        self.pos = Vector2(num * 550 + 20, 150)
+        self.width = 10
+        self.height = 100
     def move(self, w, h):
-        self.pos = self.pos.move(w * self.speed, h * self.speed)
-        if self.pos.top < 0:
-            self.pos.top = 0
-        if self.pos.bottom > 400:
-            self.pos.bottom = 400
+        self.pos.move(w * self.speed, h * self.speed)
+        if self.pos.y + self.height > 400:
+            self.pos.y = 400 - self.height
+        if self.pos.y < 0:
+            self.pos.y = 0
 
 class Ball:
     def __init__(self, image, speed):
         self.image = image
         self.speed = speed
-        self.pos = image.get_rect().move(286, 187)
+        self.pos = Vector2(286, 187)
+        self.width = 25
+        self.height = 25
         self.direction = Vector2(1, 1)
+        self.cd = 0
     def move(self):
-        self.pos = self.pos.move(self.direction.x * self.speed, self.direction.y * self.speed)
-        if self.pos.bottom > 400 or self.pos.top < 0:
+        self.pos.move(self.direction.x * self.speed, self.direction.y * self.speed)
+        if self.pos.y + self.height > 400 or self.pos.y < 0:
             self.direction.y = -self.direction.y
+    def detect_collision(self, _paddle):
+        self.cd -= 1
+        if _paddle.num == 1 and self.cd <=0 and self.pos.x + self.width > _paddle.pos.x and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height:
+            self.direction.x = -self.direction.x
+            self.cd = 3
+        if _paddle.num == 0 and self.cd <=0 and self.pos.x < _paddle.pos.x + _paddle.width and self.pos.y + self.height > _paddle.pos.y and self.pos.y < _paddle.pos.y + _paddle.height:
+            self.direction.x = -self.direction.x
+            self.cd = 3
+    
 pygame.init()
 size = width, height = 600, 400
 black = 0, 0, 0
@@ -54,10 +72,12 @@ while 1:
         paddle2.move(0, 1)
 
     ball.move()
+    ball.detect_collision(paddle1)
+    ball.detect_collision(paddle2)
 
     screen.fill(black)
-    screen.blit(paddle1.image, paddle1.pos)
-    screen.blit(paddle2.image, paddle2.pos)
-    screen.blit(ball.image, ball.pos)
+    screen.blit(paddle1.image, (paddle1.pos.x, paddle1.pos.y))
+    screen.blit(paddle2.image, (paddle2.pos.x, paddle2.pos.y))
+    screen.blit(ball.image, (ball.pos.x, ball.pos.y))
     pygame.time.delay(int(1 / 30 * 1000))
     pygame.display.flip()
